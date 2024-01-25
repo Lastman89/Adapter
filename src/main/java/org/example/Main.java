4af969ae7f4ee16bfa613c24cee5a54f8aa82a5a
Lastman89 <alexeygr89@gmail.com>
2024-01-25 18:58:06 +0300

    reformat code commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Ints calc = new IntsCalculator();
-        System.out.println(calc.sum(1,2));
-        System.out.println(calc.mult(2,2));
+        System.out.println(calc.sum(1, 2));
+        System.out.println(calc.mult(2, 2));
         System.out.println(calc.pow(2, 3));
     }
 }
